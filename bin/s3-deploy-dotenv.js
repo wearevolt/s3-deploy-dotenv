8c2c9aafb67fa44705d38dd3d28d2be5d84bce14
Staltec <staltec@gmail.com>
2016-07-05 11:48:37 +0700

    fix gzip ext bug

--- bin/s3-deploy-dotenv.js
+++ bin/s3-deploy-dotenv.js
@@ -5,34 +5,6 @@ require('dotenv').config();
 var deploy = require('../lib');
 var env = process.env;
 
-console.log('Start upload files to', env.S3_BUCKET);
-
-deploy({
-    // Required
-    region:          env.S3_REGION,
-    accessKeyId:     env.S3_ACCESS_KEY_ID,
-    secretAccessKey: env.S3_SECRET_ACCESS_KEY,
-    bucket :         env.S3_BUCKET,
-
-    // optional
-    localDir:        env.S3_LOCAL_DIR,
-    remoteDir:       env.S3_PREFIX,
-
-    maxAsyncStreams: env.S3_MAX_ASYNC_STREAMS|0,
-    uploadConcurrentParts: env.S3_UPLOAD_CONCURRENT_PARTS|0,
-    uploadMaxPartSize:     env.UPLOAD_MAX_PART_SIZE|0,
-    defaultContentType:    env.S3_DEFAULT_CONTENT_TYPE|0,
-
-    // gzip (optional)
-    gzipLevel:       env.S3_GZIP_LEVEL|0,
-    gzipExtensions: (env.S3_GZIP_EXTENSIONS || '').split(/ *, */)
-
-}, function (err) {
-    console.log('\n', err ? 'Error: ' + err : 'done');
-});
-
-
-/*
 
 deploy.config({
     // Required
@@ -52,7 +24,7 @@ deploy.config({
 
     // gzip (optional)
     gzipLevel:       env.S3_GZIP_LEVEL|0,
-    gzipExtensions: (env.S3_GZIP_EXTENSIONS || '').split(/ *, *!/),
+    gzipExtensions: (env.S3_GZIP_EXTENSIONS || '').split(/ *, */)
 
 });
 
@@ -64,13 +36,16 @@ deploy.on('error', function (err) {
     console.log('\nError: ' + err);
 });
 
-deploy.on('upload', function (file, percent) {
-    console.log((' '+percent+ '%', file.fullPath, '->', file.url);
+deploy.on('upload', function (file, percent, details) {
+    console.log(' ' + percent.toFixed(0).slice(-3) +  '%', file.fullPath, '->', file.url);
+    if (env.S3_VERBOSE) {
+        console.log(' DEBUG>', details, '\n');
+    }
 });
 
-deploy.on('complete', function () {
-    console.log('\ndone');
+deploy.on('complete', function (count) {
+    console.log('\n', count, 'files uploaded');
 });
+
 
 deploy.upload();
-*/
